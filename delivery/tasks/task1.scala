e224c4959a1b6c701cf6a5dcac9ebd2983b9f9c4
Lucas Emmes <79790657+LucasEmmes@users.noreply.github.com>
2021-10-29 12:43:23 +0200

    Finished task1.scala :)
    :)

--- delivery/tasks/task1.scala
+++ delivery/tasks/task1.scala
@@ -1,14 +1,46 @@
 object Task1 {
     def main(args: Array[String]) {
-        
-        var numbers:Array[String] = new Array[String](50)
-        
-        for ( i <- 0 to (numbers.length - 1)) {
-            numbers[i] = i+1
+
+        // A
+        var numbers:Array[Int] = new Array[Int](50);
+        for ( i <- 0 to numbers.length - 1) {
+            numbers(i) = i+1;
         }
-        
-        println(numbers)
+        println(numbers.mkString(" "));
+    
+        // B
+        def sum(list:Array[Int]): Int = {
+            var sum = 0;
+            for (i <- 0 to list.length-1) {
+                sum += list(i);
+            }
+            return sum;
+        }
+        println(sum(numbers));
+    
+        // C
+        def sum_recursive(list:Array[Int], index:Int):Int = {
+            if (index < list.length) {
+                return list(index) + sum_recursive(list, index+1)
+            } else {
+                return 0
+            }
+        }
+        println(sum_recursive(numbers,0));
+    
+        // D
+        def fibonacci(n:Int):BigInt = {
+            // Shorter version but we're afraid of getting deduction for """"unreasonable indentation""""
+            // return if (n<2) n else fib(n-1) + fib(n-2);
+            
+            if (n < 2) {
+                return n;
+            } else {
+                return fib(n-1) + fib(n-2);
+            }
+        }
+        println(fibonacci(15));
+
     }
 
-
-})
+}
