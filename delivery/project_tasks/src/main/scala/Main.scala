6cae5c30936d38c81f799378e8d9f6c21097cff3
tordstr <tordstr@stud.ntnu.no>
2021-10-29 17:05:30 +0200

    Some of task2

--- delivery/project_tasks/src/main/scala/Main.scala
+++ delivery/project_tasks/src/main/scala/Main.scala
@@ -1,5 +1,76 @@
+import scala.concurrent.Future
+import scala.concurrent.Await
+
 
 object Main extends App {
+
+
+        def generate50():Array[Int] = {
+        
+        var numbers:Array[Int] = new Array[Int](50)
+        
+        for (i <- 0 to (numbers.length - 1)) {
+            numbers(i) = i+1
+            // println(numbers(i))
+        }
+        return numbers
+    }
+
+    def sum_array(a:Array[Int]): Int = {
+        var total = 0;
+        for (i <- 0 to (a.length-1)) {
+            total += a(i)
+        }
+        return total
+    }
+
+    def sum_recursive(a:Array[Int], index:Int = 0): Int = {
+        if (index < a.length) {
+            return a(index) + sum_recursive(a, index+1)
+        } else {
+            return 0
+        }
+    }
+
+    def fib(n:Int): BigInt = {
+
+        if (n <= 0) {
+            return 0
+        }
+        if (n == 1) {
+            return 1
+        }
+        
+
+        var a:BigInt = fib(n-1)
+        var b:BigInt = fib(n-2)
+
+        return a + b
+    }
+
+    def create_thread(function: () => Unit): Thread = {
+
+        val t = new Thread {
+            override def run {
+                return function()
+            }
+        }
+        return t
+    }
+
+    private var counter: Int = 0
+    
+    def increaseCounter(): Unit = this.synchronized {
+        val current = counter
+        // possible read here
+        Thread.sleep(100)
+        counter = current + 1
+    }
+
+    def printCounter() = {
+        println(counter)
+    }
+
 
     def thread(body: => Unit): Thread = {
         val t = new Thread {
@@ -8,5 +79,47 @@ object Main extends App {
         t.start
         t
     }
+
+    // var a:Array[Int] = generate50()
+    // println(sum_array(a))
+    // println(sum_recursive(a))
   
+    // println(fib(15))
+
+    // var t1 = create_thread(increaseCounter)
+    // var t2 = create_thread(increaseCounter)
+    // var t3 = create_thread(printCounter)
+    
+    // t1.start()
+    // t2.start()
+    // Thread.sleep(1000)
+    // t3.start()
+
+
+}
+
+object FirstObj {
+  lazy val initialState = 42
+  lazy val start = SecondObj.initialState
+}
+
+object SecondObj {
+  lazy val initialState = FirstObj.initialState
+}
+
+object Deadlock extends App {
+    implicit val ec = ExecutionContext.global
+
+    def run = {
+        val result = Future.sequence(Seq(
+        Future {
+            FirstObj.start
+        },
+        Future {
+            SecondObj.initialState
+        }
+    ))
+    Await.result(result, 10.second)
+    }
+    run
 }
