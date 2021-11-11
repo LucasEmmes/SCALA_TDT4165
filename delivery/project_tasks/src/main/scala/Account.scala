638544e5e7778eafc1957983692a741f4b8f8b5d
tordstr <tordstr@stud.ntnu.no>
2021-11-11 15:13:34 +0100

    task 1.2

--- delivery/project_tasks/src/main/scala/Account.scala
+++ delivery/project_tasks/src/main/scala/Account.scala
@@ -9,9 +9,27 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = ???
-    def deposit (amount: Double): Unit = ???
-    def getBalanceAmount: Double       = ???
+    def withdraw(amount: Double): Either[Unit, String] = this.synchronized {
+        if (this.getBalanceAmount() < amount) {
+            return "shit fucked yo"
+        }
+        if (amount < 0) {
+            return "amount < 0 you stupid fack"
+        }
+
+        balance.amount -= amount
+    }
+
+    def deposit (amount: Double): Either[Unit, String] = this.synchronized {
+        if (amount < 0) {
+            return "amount < 0 you stupid fack"
+        }
+        balance.amount += amount
+    }
+
+    def getBalanceAmount: Double = {
+        return balance.amount
+    }
 
     def transferTo(account: Account, amount: Double) = {
         bank addTransactionToQueue (this, account, amount)
